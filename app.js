9c864a7d17ecfca727997c37bdef73efe98450ab
lLiszt <kocoro.rororo@gmail.com>
2018-07-16 13:03:32 +0700

    test

--- app.js
+++ app.js
@@ -20,13 +20,13 @@ function reply(reply_token, msg) {
         'Authorization': 'Bearer {ffoSQHv7DNQl8fCqtoCR7aZlf+wHzJcNd7K9crw+nIcZcTepvAZ3933vuwEwSnUxg41iHupe5eZHvPkYDGxLJEcwZUlA/+kS6bWbL0OtbsYC1b6/NfVnXX09z4uUhzHvza4UrjWsRx8nAsA1vsLHPAdB04t89/1O/w1cDnyilFU=}'
 
     }
-    
+    var name = 'Mind';
     let body = JSON.stringify({
         replyToken: reply_token,
         messages: [
         {
             type: 'text',
-            text: msg
+            text: msg+ ' testttt '+name
         }
 
 
