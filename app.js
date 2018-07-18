72b36fb46ffbc9b4d5ae44112ef320dd15da7785
lLiszt <kocoro.rororo@gmail.com>
2018-07-18 13:33:10 +0700

    test

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Echo reply
-
+"use strict";
 
 
 const express = require('express')
@@ -9,7 +9,7 @@ const AIMLParser = require('aimlparser')
 const path = require('path');
 const cp = require('child_process');
 const fs = require('fs');
-
+const line = require('@line/bot-sdk');
 
 
 
@@ -27,7 +27,8 @@ aimlParser.load(['./test-aiml.xml'])
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
+app.use('/static', express.static('static'));
+app.use('/downloaded', express.static('downloaded'));
 
 app.post('/webhook', (req, res) => {
 
