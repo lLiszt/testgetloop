7bf6fd4020c0a195bbb34d78b4b367b39983a7c1
lLiszt <kocoro.rororo@gmail.com>
2018-07-18 14:09:26 +0700

    test

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
-const AIMLParser = require('aimlparser')
+
 const path = require('path');
 const cp = require('child_process');
 const fs = require('fs');
@@ -23,7 +23,7 @@ const aimlParser = new AIMLParser({ name:'HelloBot' })
 
 
 
-aimlParser.load(['./test-aiml.xml'])
+
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
