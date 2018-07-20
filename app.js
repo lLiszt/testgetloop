cecf04452f1d4db308f8387abe0f93ca1aa9158a
lLiszt <kocoro.rororo@gmail.com>
2018-07-20 11:22:29 +0700

    test

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const request = require('request')
 const path = require('path');
 const cp = require('child_process');
 const line = require('@line/bot-sdk');
+const axios = require('axios');
 
 require('dotenv').config();
 
@@ -81,6 +82,7 @@ function handleEvent(event) {
 
     case 'message':
       const message = event.message;
+      usercon(event.source.userId);
 
       switch (message.type) {
         case 'text':
@@ -350,6 +352,17 @@ function handleText(message, replyToken, source) {
           });
       });
   }
+}
+const usercon = async (userId) => {
+    const res = await axios.get('https://nodejs.org/ja/');
+    const item = res.data;
+    const version = item.match(/最新版"  data-version="(.*?)">/)[1]; //正規表現で(無理やり)取得
+    console.log(version);
+
+    await client.pushMessage(Ub8cad621e155de8753e6ebddc9db3d68, {
+        type: 'text',
+        text: `Fish is good`,
+    });
 }
 
 app.set('port', (process.env.PORT || 4000));
