6972621373f428eb428a8290a86eab98ee36791d
lLiszt <kocoro.rororo@gmail.com>
2018-07-23 09:05:28 +0700

    test

--- app.js
+++ app.js
@@ -146,7 +146,7 @@ function handleText(message, replyToken, source) {
       return replyText(replyToken, 'Bot can\'t use profile API without user ID');
 
        case 'Push':
-          return client.push('Ub8cad621e155de8753e6ebddc9db3d68', ['Hey!', 'สวัสดี ' + String.fromCharCode(0xD83D, 0xDE01)]);
+          return client.replyText('Ub8cad621e155de8753e6ebddc9db3d68', ['Hey!', 'สวัสดี ' + String.fromCharCode(0xD83D, 0xDE01)]);
     
 
     case 'buttons':
