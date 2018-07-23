548b3fc28f1e381af135ec40b9f9429e512d08a5
lLiszt <kocoro.rororo@gmail.com>
2018-07-23 10:44:13 +0700

    test

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ require('dotenv').config();
 
 const app = express()
 
+
 const config = {
   channelAccessToken: (process.env.CHANNEL_ACCESS_TOKEN || 'ffoSQHv7DNQl8fCqtoCR7aZlf+wHzJcNd7K9crw+nIcZcTepvAZ3933vuwEwSnUxg41iHupe5eZHvPkYDGxLJEcwZUlA/+kS6bWbL0OtbsYC1b6/NfVnXX09z4uUhzHvza4UrjWsRx8nAsA1vsLHPAdB04t89/1O/w1cDnyilFU='),
   channelSecret: (process.env.CHANNEL_SECRET || 'c9865f7627be2bdc7a37a411b99e0d16'),
@@ -24,6 +25,7 @@ const client = new line.Client(config);
 
 app.use('/static', express.static('static'));
 app.use('/downloaded', express.static('downloaded'));
+ let user = req.body.events[0].source.userId
 
 app.post('/webhook', line.middleware(config), (req, res) => {
   // req.body.events should be an array of events
