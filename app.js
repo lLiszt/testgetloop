12d5d5260d26bbf6266e44a698f2e9a7292d110c
lLiszt <kocoro.rororo@gmail.com>
2018-07-23 10:30:22 +0700

    test

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ function replyText(reply_token, msg, user) {
 
   let body = JSON.stringify({
     replyToken: reply_token,
-    to:'Ub8cad621e155de8753e6ebddc9db3d68',
+
     messages: [{
       type: 'text',
       text: msg + " --> " + user
@@ -59,7 +59,7 @@ function replyText(reply_token, msg, user) {
   })
 
   request.post({
-    url: 'https://api.line.me/v2/bot/message/push',
+    url: 'https://api.line.me/v2/bot/message/reply',
     headers: headers,
     body: body
   }, (err, res, body) => {
