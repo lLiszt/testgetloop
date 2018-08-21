1155abca851678e0aff6a2130015fcc4711e4d73
kotkorn <phophun-kot@hotmail.com>
2018-08-21 10:19:07 +0700

    test

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const AIMLParser = require('aimlparser')
 
 require('dotenv').config();
 
-
+aimlParser.load(['http://20.188.101.72:8080/api/data/po_list/2003-11-04'])
 
 const app = express()
 
@@ -50,26 +50,7 @@ app.post('/webhook', line.middleware(config), (req, res) => {
 
 });
 
-//url
-var url = require('url');
-var adr = 'http://20.188.101.72:8080/api/data/po_list/2003-11-04';
-var q = url.parse(adr, true);
 
-console.log(q.host); //returns 'localhost:8080'
-console.log(q.pathname); //returns '/default.htm'
-console.log(q.search); //returns '?year=2017&month=february'
-
-var qdata = q.query; //returns an object: { year: 2017, month: 'february' }
-console.log(qdata.PONumber); //returns 'february'
-console.log(qdata.CreateDate);
-console.log(qdata.Name); 
-console.log(qdata.Vendor); 
-
-var PONumber = PONumber.qdata
-var CreateDate = CreateDate.qdata
-var Name = Name.qdata
-var Vendor = Vendor.qdata
-//end url
 
 const replyText = (token, texts) => {
   texts = Array.isArray(texts) ? texts : [texts];
@@ -215,34 +196,9 @@ async function handleText(message, replyToken, source) {
                   { label: '言 hello2', type: 'postback', data: 'hello こんにちは', text: 'hello こんにちは' },
                   { label: 'Say message', type: 'message', text: 'Rice=米' },
                 ],
-              },
-               {
-                thumbnailImageUrl: buttonsImageURL,
-                title: 'hoge',
-                text: 'fuga',
-                actions: [
-                  { label: '言 hello2', type: 'postback', data: 'hello こんにちは', text: 'hello こんにちは' },
-                  { label: 'Say message', type: 'message', text: 'Rice=米' },
-                ],
-              },
-               {
-                thumbnailImageUrl: buttonsImageURL,
-                title: 'hoge',
-                text: 'fuga',
-                actions: [
-                  { label: '言 hello2', type: 'postback', data: 'hello こんにちは', text: 'hello こんにちは' },
-                  { label: 'Say message', type: 'message', text: 'Rice=米' },
-                ],
-              },
-               {
-                thumbnailImageUrl: buttonsImageURL,
-                title: 'hoge',
-                text: 'fuga',
-                actions: [
-                  { label: '言 hello2', type: 'postback', data: 'hello こんにちは', text: 'hello こんにちは' },
-                  { label: 'Say message', type: 'message', text: 'Rice=米' },
-                ],
-              },
+              }
+               
+             
             ],
           },
         }
